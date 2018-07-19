c05660ed01fcb61bc1eb9f2102c1fe1e1c3be226
CharmingMother <nawhpongmaji1@gmail.com>
2018-07-19 14:05:48 -0500

    Create Video2-status.py

---
+++ Video2-status.py
@@ -0,0 +1,49 @@
+import discord
+from discord.ext import commands
+import asyncio
+import random
+
+
+
+
+
+yakumo=commands.Bot(command_prefix='y.')
+
+
+
+#BT DEFAULT WIHTOUT A TYPE IT WILL EQUAL TO PLAYING
+#TYPE 3 = WATCHING
+async def watch():
+    #YOU CAN MAKE YOUR OWN LIST, BUT FEEL FREE TO USE MINE
+    watch_list = ['Man vs Wild', 'Supernatural', 'Trick2g', 'RIOT GAMES', 'You', '@everyone', 'Silent Hill', 'The Grudge', 'Mouse Trap Mondays:Shawn Woods']
+    while True:
+        await asyncio.sleep(8)
+        await yakumo.change_presence(game=discord.Game(name=random.choice(watch_list),type=3))
+
+#TYPE 2 = LISTENING
+async def listen():
+    #YOU CAN MAKE YOUR OWN LIST, BUT FEEL FREE TO USE MINE
+    listen_list = ['Alan Walker Faded', 'Trick2g\'s Scream', 'Apink','Twice:Knock Knock', 'Let it go:Meiko', 'I Dreamed a Dream, Susan Boyle']
+    while True:
+        await yakumo.change_presence(game=discord.Game(name=random.choice(listen_list), type=2))
+        await asyncio.sleep(15)
+
+
+
+
+
+#COLLECTS THE TWO FUNCTIONS INTO ONE
+async def change_status():
+    pass
+
+
+@yakumo.event
+async def on_ready():
+    print(yakumo.user.name)
+    #RUN YOUR BACKGROUND TASK HERE
+
+
+
+
+#REPLACE BOT_TOKEN WITH YOUR ACTUAL BOT TOKEN
+yakumo.run('BOT_TOKEN')
