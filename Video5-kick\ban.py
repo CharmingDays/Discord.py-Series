02eef0f823d4f40ed5558f67698c9a3b83e4b3f0
CharmingMother <nawhpongmaji1@gmail.com>
2018-10-09 09:17:59 -0500

    Update Video5-kick\ban.py

--- Video5-kick\ban.py
+++ Video5-kick\ban.py
@@ -1,7 +1,7 @@
 import discord
 import asyncio
 from discord.ext import commands
-
+from discord.utils import *
 
 bot=commands.Bot(command_prefix='y.)
 
@@ -18,11 +18,15 @@ async def on_ready():
 
 @bot.command(pass_context=True)
 async def kick(con,user:discord.Member):
-    pass
+    name=user.name
+    await bot.kick(user)
+    await bot.say("User {} has been kicked by {}".format(name,con.message.author.name))
 
-
-
-
+@bot.command(pass_context=True)
+async def ban(con,user:discord.Member):
+    name=user.name
+    await bot.ban(user)
+    await bot.say("{} has been banned by {} from {}".format(name,con.message.author.name,con.message.server.name))
 
 
 
