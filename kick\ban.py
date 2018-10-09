6c7d61c695a18dd676dae7c9d5393afb33fadd08
CharmingMother <nawhpongmaji1@gmail.com>
2018-10-09 09:07:06 -0500

    Create kick\ban.py

---
+++ kick\ban.py
@@ -0,0 +1,28 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+
+bot=commands.Bot(command_prefix='y.)
+
+
+
+
+
+
+@bot.event
+async def on_ready():
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot.run('BOT TOKEN HERE')
